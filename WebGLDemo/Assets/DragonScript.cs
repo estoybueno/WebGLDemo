1f763d6621bf26ce5530fd16bb74fd2056353c9a
Kevin Everett <Kevin.Everett@tietronix.com>
2018-11-06 08:06:12 -0600

    init

---
+++ WebGLDemo/Assets/DragonScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonScript : MonoBehaviour {
+	[Tooltip("So long as stateName==clipName, the clips here will play OnClick")]
+	public List<AnimationClip> clips;
+	[Tooltip("The particle system representing the dragon's breath attack")]
+	public GameObject breathFX;
+	private Animator anim;
+	private AnimationClip nextClip;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		cycleClips();
+	}
+	private void cycleClips(){//update nextClip, and move it from front to back of list
+		nextClip = clips[0];
+		clips.RemoveAt (0);
+		clips.Add (nextClip);
+	}
+
+
+	//when user clicks model, plays next anim clip, followed by idle clip
+	void OnMouseUpAsButton(){//doesnt work with just meshcollider so i added a boxcollider
+		StopAllCoroutines();
+		StartCoroutine("playClipThenIdle",nextClip);
+		cycleClips();
+	}
+
+	IEnumerator playClipThenIdle(AnimationClip c){
+		anim.Play (c.name);
+		print ("playing clip " + c.name);
+		if(c.name == "atk02"){//activate the particle system asssociated with the animation
+			yield return new WaitForSecondsRealtime (1f);
+			breathFX.SetActive (true);
+			yield return new WaitForSecondsRealtime (c.averageDuration - 1.3f);
+		}
+		else{
+			yield return new WaitForSecondsRealtime (c.averageDuration);
+		}
+		breathFX.SetActive (false);
+		anim.Play ("idle");
+	}
+}
---
+++ WebGLDemo/Assets/followSourcePt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followSourcePt : MonoBehaviour {
+	public Vector3 targetPos;
+	public Vector3 targetRot;
+	private Quaternion targetRotQuat = new Quaternion();
+	private Quaternion initRot;
+	private Vector3 initPos;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		initPos = this.transform.position;
+		initRot = this.transform.rotation;
+		targetRotQuat.eulerAngles = targetRot;
+		rb = GetComponent<Rigidbody> ();
+		//StartCoroutine ("positionOverTime");
+	}
+	void OnEnable(){
+		StartCoroutine ("positionOverTime");
+	}
+	IEnumerator positionOverTime(){//move from init to target over 1 second
+		while(this.transform.position != targetPos){
+			this.transform.position = Vector3.Lerp(this.transform.position, targetPos, 0.01f) ;
+			this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotQuat, 0.01f) ;
+			yield return new WaitForSecondsRealtime(0.01f);
+		}
+
+	}
+	void OnDisable(){
+		this.transform.position = initPos;//may not be necessary
+		this.transform.rotation = initRot;
+	}
+}
